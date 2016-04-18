96ed09c12b73a8f5e25ac2ca33124dd322e6e1cf
davidjraso <davidjraso@wustl.edu>
2016-04-18 18:18:27 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,7 +12,7 @@ function question(questionText) {
 // call the QUESTION function with the order options
 question("What do you want to order?");
 question("What protein do you want?");
-question("What salsa do you want?");
+question("What cheese do you want?");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
