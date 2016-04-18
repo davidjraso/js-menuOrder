47b361d4b41f51890594ebb4c65bfe40a7772663
davidjraso <davidjraso@wustl.edu>
2016-04-18 18:16:52 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,12 +4,15 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(questionText) {
+ var answerInput = prompt(questionText);
+  order += answerInput;
 }
 
 // call the QUESTION function with the order options
-question();
+question("What do you want to order?");
+question("What protein do you want?");
+question("What salsa do you want?");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
