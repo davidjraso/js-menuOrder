3803a60408c38ffe8c3bbe946688e9005296991d
davidjraso <davidjraso@wustl.edu>
2016-04-18 18:25:46 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,19 +1,45 @@
 // begin with a blank/new order
 var order = '';
+var main = '';
+var last = '';
 
 
 // build a function to ask questions
 
 function question(questionText) {
  var answerInput = prompt(questionText);
- //answerInput = answerInput + " "
   order += answerInput + ' ';
 }
 
+function questionMain(questionText) {
+ var answerInput = prompt(questionText);
+  main += answerInput;
+}
+
+function questionLast(questionText) {
+ var answerInput = prompt(questionText);
+  last += answerInput;
+}
+
+function questionRespond(questionText, food) {
+ var answerInput = prompt(questionText);
+ if ( answerInput.toUpperCase() === "YES" ) {
+  order += food + ' ';
+ }
+  main += answerInput;
+}
+
 // call the QUESTION function with the order options
-question("What do you want to order?");
+questionMain("What do you want to order?");
 question("What protein do you want?");
-question("What cheese do you want?");
+questionRespond("Do you want beans?", "beans");
+questionRespond("Do you want rice?", "rice");
+questionRespond("Do you want vegetables?", "vegetables");
+questionRespond("Do you want cheese?", "cheese");
+questionRespond("Do you want lettuce?", "lettuce");
+question("Which type of salsa do you want?");
+questionLast("Do you want guac?", "guac");
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+document.write("<p>You ordered a: " + main + " with " + order + " and " + last + "<p>");
+document.write("<p>Have a great day!<p>");
