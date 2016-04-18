33fdc3c1d6fd68e881ba60e0d46a2214c7efd956
davidjraso <davidjraso@wustl.edu>
2016-04-18 18:32:28 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -6,9 +6,9 @@ var last = '';
 
 // build a function to ask questions
 
-function question(questionText) {
+function questionProtein(questionText) {
  var answerInput = prompt(questionText);
-  order += answerInput + ' ';
+  protein += answerInput + ' ';
 }
 
 function questionMain(questionText) {
@@ -34,15 +34,14 @@ function questionRespond(questionText, food) {
 
 // call the QUESTION function with the order options
 questionMain("What do you want to order?");
-question("What protein do you want?");
+questionProtein("What protein do you want?");
 questionRespond("Do you want beans? (yes or no)", "beans");
 questionRespond("Do you want rice? (yes or no)", "rice");
 questionRespond("Do you want vegetables? (yes or no)", "vegetables");
 questionRespond("Do you want cheese? (yes or no)", "cheese");
 questionRespond("Do you want lettuce? (yes or no)", "lettuce");
-question("Which type of salsa do you want?");
 questionLast("Do you want guac?", "guac");
 
 // output their order
-document.write("<p>You ordered a: " + main + " with " + order + " and " + last + "<p>");
+document.write("<p>You ordered a: " + main + " with " + protein + order + " and " + last + "<p>");
 document.write("<p>Have a great day!<p>");
